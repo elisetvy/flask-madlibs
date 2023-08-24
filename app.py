2946bd54ec6048e20396a633af80bd5868d53437
Gabe Yoro <gabrielyoro@gmail.com>
2023-08-23 17:01:19 -0700

    Added solution up to further study

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+# descriptive verb noun
+def index():
+    prompts = silly_story.prompts
+    # unnecessary
+    template = silly_story.template
+    return render_template("questions.html",
+                           prompts=prompts,
+                           template=template
+                           )
+
+
+@app.get('/results')
+# descriptive
+def handle_results():
+    story = silly_story.get_result_text(request.args)
+    return render_template('results.html', text=story)
